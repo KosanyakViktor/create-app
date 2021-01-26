2aff6200b9c52e9af6d2696b7ec6fec0eb50b393
Viktor Kosanyak <kosanyakviktor@gmail.com>
2021-01-26 17:20:08 +0200

    commit-one

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
+// //index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./index.css"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const rootElement = document.querySelector('#root');
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// const renderGreeting = elem => {
+//     const containerElem = document.createElement('div');
+//     containerElem.classList.add('greeting');
+    
+//     const titleElem = document.createElement('h1');
+//     titleElem.classList.add('greeting__title');
+//     titleElem.textContent = 'Hello, world!'; 
+    
+//     const textElem = document.createElement('p');
+//     textElem.classList.add('greeting__text');
+//     textElem.textContent = 'I\'m learning React';
+
+//     containerElem.append(titleElem, textElem);
+
+//     elem.append(containerElem);
+// };
+
+// renderGreeting(rootElement);
+
+const element = (
+  <div className="greeting"> 
+    <div className="greeting__title">Hello, world!</div>
+    <div className="greeting__text">I'm learning React</div>
+  </div>
+)
+
+
+ReactDOM.render(element, rootElement);
